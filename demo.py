2bda71ae1f83fb0d8ea59e42a15b0b7161716d0b
SyedaMehmilZehra <smehmilzehra@gmail.com>
2023-07-08 00:48:45 -0700

    New Message

---
+++ demo.py
@@ -0,0 +1,43 @@
+# print("UMM E HANI")
+# a=10  
+# b=10
+# print(a+b)
+# a= (10+10)
+# b=(20)
+# print(a+b)
+# a=("umme")
+# print(a)
+# x=10*10-4+6/10
+# print(x)
+# x=("34+55-60")
+# print(x)
+# print("UMM E HANI you are the best")
+# age =30
+# if age == 30:
+#     print("This is the correct age")
+# else:
+#     print("this is not a correct age")    
+
+
+
+perc= int(input())
+if perc <= 100 and perc >= 80:
+    print ("A+")
+elif perc < 80 and perc >= 70:
+    print ("A")
+elif perc < 70 and perc >= 60:
+    print ("B")
+elif perc < 60 and perc >= 50:
+    print ("C")
+elif perc < 50 and perc >= 40:
+    print ("D")
+elif perc < 40 and perc >= 33:
+        print ("E")
+elif perc > 100 or perc < 1:
+    print ("not appropiate") 
+else:
+ print("Fail")
+ 
+     
+
+
---
+++ today.py
@@ -0,0 +1,10 @@
+print("Hello World")
+
+math_mark = int(input("Enter math marks out of 100"))
+physics_mark = int(input("Enter physics marks out of 100"))
+chemistry_mark = int(input("Enter chemistry marks out of 100"))
+urdu_mark = int(input("Enter urdu marks out of 100"))
+bio_mark = int(input("Enter bio marks out of 100"))
+
+total = 500
+obtained_marks = ''
